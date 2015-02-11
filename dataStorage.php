d0b4b36469cc4ac6e4daf98b209d1f907f1e0e56
Rob Waltman <robertdwaltman@gmail.com>
2015-02-11 01:22:56 -0500

    updated files

--- dataStorage.php
+++ dataStorage.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-include 'storedInfo.php';
+include 'storedInfo.php'; //password file, permissions set to exclude everyone from seeing it
 ?>
 <!DOCTYPE = HTML>
 <html>
@@ -13,7 +13,7 @@ include 'storedInfo.php';
 	<?php
 
 
-	$mysqli = new mysqli("oniddb.cws.oregonstate.edu", "waltmanr-db", $password, "waltmanr-db");
+	$mysqli = new mysqli("oniddb.cws.oregonstate.edu", "waltmanr-db", $password, "waltmanr-db"); //initialize the connection
 
 	if($mysqli->connect_errno){
 		echo "Connect failed: (" . $mysqli->connect_errono . ") " . $mysqli->connect_error;
@@ -25,7 +25,7 @@ include 'storedInfo.php';
 	echo "<form method='post' action='dataStorage.php'>";
 	echo "Title: <input type='text' name='inputName'>  Category: <input type='text' name='inputCategory'>  Length: <input type='text' name='inputLength'>";
 	echo "<input type='submit' value='Submit'><br>";
-	echo "</form>";
+	echo "</form>"; //new input form
 
 	if(isset($_POST['inputName'])){
 		addNewVideo();
@@ -37,7 +37,7 @@ include 'storedInfo.php';
 		checkOutVideo();
 	} else if (isset($_POST['Remove'])){
 		removeVideo();
-	} 
+	} //these functions test to see if the user kicked any commands back by using the buttons
 	createFilter();
 	drawTable();
 	?>
@@ -52,16 +52,16 @@ include 'storedInfo.php';
 		global $mysqli;
 		$res;
 		echo "<table border='1' cellpadding='2'> <tr> <td>ID#</td> <td>Name</td> <td>Category</td> <td>Length</td> <td>Rented?</td> <td>Check Out/Return</td> <td>Remove</td></tr>";
-		if (isset($_POST['category']) && $_POST['category'] != "all"){
+		if (isset($_POST['category']) && $_POST['category'] != "all"){ //if the user is using a filter, use this
 			$sanitized = $mysqli->real_escape_string($_POST['category']);
 			$res = $mysqli->query("SELECT * FROM vidStore WHERE category = '$sanitized'");
 		} else {
-			$res = $mysqli->query("SELECT * FROM vidStore WHERE 1");
+			$res = $mysqli->query("SELECT * FROM vidStore WHERE 1"); //otherwise use this
 		}
 		
 		$res->data_seek(0);
 		while($row = $res->fetch_assoc()){
-			if($row['rented'] == 1){
+			if($row['rented'] == 1){ //this determines what to populate the return/check out buttons with
 				$rentStatus = "Checked Out";
 				$buttonText = "Return";
 			} else {
@@ -74,6 +74,7 @@ include 'storedInfo.php';
 			<button type='submit' name='" . $buttonText . "' value='" . $row['id'] . "'>" . $buttonText . "</button></td></form><td>
 			<form method='post' style='margin: 0; text-align: center;' action='dataStorage.php'>
 			<button type='submit' name='Remove' value='" . $row['id'] . "'>Remove</button></td></form></tr>";
+			//add everything in one massive echo statement, organized in a table
 		}
 		echo "</table>";
 
@@ -85,7 +86,7 @@ include 'storedInfo.php';
 		} else if (!empty($_POST['inputLength']) && !is_numeric($_POST['inputLength'])){
 			echo "ERROR: Length must be a number!<br>";
 		} else if (!empty($_POST['inputLength']) && $_POST['inputLength'] < 0){
-			echo "ERROR: Length must be a positive number!<br>";
+			echo "ERROR: Length must be a positive number!<br>"; //error checking for new movies
 		} else {
 			global $mysqli;
 			if (!($stmt = $mysqli->prepare("INSERT INTO vidStore(name, category, length) VALUES (?, ?, ?)"))) {
@@ -107,7 +108,7 @@ include 'storedInfo.php';
 
 	function returnVideo(){
 		global $mysqli;
-		$stmt = $mysqli->prepare("UPDATE vidStore SET rented = NOT rented WHERE id = (?)");
+		$stmt = $mysqli->prepare("UPDATE vidStore SET rented = NOT rented WHERE id = (?)"); //toggle the rental status
 		$stmt->bind_param('i', $_POST['Return']);
 		$stmt->execute();
 		$stmt->close();
@@ -137,32 +138,12 @@ include 'storedInfo.php';
 		$res = $mysqli->query("SELECT DISTINCT category FROM vidStore WHERE category != ''");
 		$res->data_seek(0);
 		echo "<form method='post' action='dataStorage.php'><select name='category'>";
-		while($row = $res->fetch_assoc()){
+		while($row = $res->fetch_assoc()){ //this populates the drop-down list with the possible options
 			echo "<option value='" . $row['category'] . "'>" . $row['category'] . "</option>";
 		}
 		echo "<option value='all'>All Categories</option>";
 		echo "</select><button type='submit' name='filter' value='Filter'>Filter</button></form>";
 	}
-
-
-	// if (!$mysqli->query("DROP TABLE IF EXISTS test") || !$mysqli->query("CREATE TABLE test(id INT)")) {
-	//     echo "Table creation failed: (" . $mysqli->errno . ") " . $mysqli->error;
-	// }
-
-	// /* Prepared statement, stage 1: prepare */
-	// if (!($stmt = $mysqli->prepare("INSERT INTO test(id) VALUES (?)"))) {
-	//     echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
-	// }
-
-	// /* Prepared statement, stage 2: bind and execute */
-	// $id = 1;
-	// if (!$stmt->bind_param("i", $id)) {
-	//     echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
-	// }
-
-	// if (!$stmt->execute()) {
-	//     echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-	//}
 
 	?>
 </body>
